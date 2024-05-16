c9b76894b164f88101506a469c381549bc713f6c
Idea Bosque <ideabosque@gmail.com>
2024-05-15 17:58:09 -0700

    Added input and output type.

--- openai_assistant_engine/queries.py
+++ openai_assistant_engine/queries.py
@@ -15,39 +15,58 @@ from .handlers import (
     resolve_message_handler,
     resolve_message_list_handler,
 )
+from graphene import ResolveInfo
+from typing import Dict, Any
+from .types import (
+    AskOpenAIType,
+    LastMessageType,
+    CurrentRunType,
+    AssistantType,
+    AssistantListType,
+    ThreadType,
+    ThreadListType,
+    MessageType,
+    MessageListType,
+)
 
 
-def resolve_ask_open_ai(info, **kwargs):
+def resolve_ask_open_ai(info: ResolveInfo, **kwargs: Dict[str, Any]) -> AskOpenAIType:
     return ask_open_ai_handler(info, **kwargs)
 
 
-def resolve_last_message(info, **kwargs):
+def resolve_last_message(
+    info: ResolveInfo, **kwargs: Dict[str, Any]
+) -> LastMessageType:
     return last_message_handler(info, **kwargs)
 
 
-def resolve_current_run(info, **kwargs):
+def resolve_current_run(info: ResolveInfo, **kwargs: Dict[str, Any]) -> CurrentRunType:
     return current_run_handler(info, **kwargs)
 
 
-def resolve_assistant(info, **kwargs):
+def resolve_assistant(info: ResolveInfo, **kwargs: Dict[str, Any]) -> AssistantType:
     return resolve_assistant_handler(info, **kwargs)
 
 
-def resolve_assistant_list(info, **kwargs):
+def resolve_assistant_list(
+    info: ResolveInfo, **kwargs: Dict[str, Any]
+) -> AssistantListType:
     return resolve_assistant_list_handler(info, **kwargs)
 
 
-def resolve_thread(info, **kwargs):
+def resolve_thread(info: ResolveInfo, **kwargs: Dict[str, Any]) -> ThreadType:
     return resolve_thread_handler(info, **kwargs)
 
 
-def resolve_thread_list(info, **kwargs):
+def resolve_thread_list(info: ResolveInfo, **kwargs: Dict[str, Any]) -> ThreadListType:
     return resolve_thread_list_handler(info, **kwargs)
 
 
-def resolve_message(info, **kwargs):
+def resolve_message(info: ResolveInfo, **kwargs: Dict[str, Any]) -> MessageType:
     return resolve_message_handler(info, **kwargs)
 
 
-def resolve_message_list(info, **kwargs):
+def resolve_message_list(
+    info: ResolveInfo, **kwargs: Dict[str, Any]
+) -> MessageListType:
     return resolve_message_list_handler(info, **kwargs)
