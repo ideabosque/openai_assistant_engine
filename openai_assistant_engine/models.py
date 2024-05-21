1b2682affed9a93a7e0b63a1cad570156e24095e
Idea Bosque <ideabosque@gmail.com>
2024-05-21 10:13:52 -0700

    Updated types and modules.

--- openai_assistant_engine/models.py
+++ openai_assistant_engine/models.py
@@ -14,43 +14,41 @@ from pynamodb.attributes import (
 )
 from pynamodb.indexes import GlobalSecondaryIndex, LocalSecondaryIndex, AllProjection
 from silvaengine_dynamodb_base import BaseModel
-from typing import List, Optional
-from datetime import datetime
 
 
 class AssistantModel(BaseModel):
     class Meta(BaseModel.Meta):
         table_name = "oae-assistants"
 
-    assistant_type: str = UnicodeAttribute(hash_key=True)
-    assistant_id: str = UnicodeAttribute(range_key=True)
-    assistant_name: str = UnicodeAttribute()
-    functions: List[str] = ListAttribute()
-    updated_by: str = UnicodeAttribute()
-    created_at: datetime = UTCDateTimeAttribute()
-    updated_at: datetime = UTCDateTimeAttribute()
+    assistant_type = UnicodeAttribute(hash_key=True)
+    assistant_id = UnicodeAttribute(range_key=True)
+    assistant_name = UnicodeAttribute()
+    functions = ListAttribute()
+    updated_by = UnicodeAttribute()
+    created_at = UTCDateTimeAttribute()
+    updated_at = UTCDateTimeAttribute()
 
 
 class ThreadModel(BaseModel):
     class Meta(BaseModel.Meta):
         table_name = "oae-threads"
 
-    assistant_id: str = UnicodeAttribute(hash_key=True)
-    thread_id: str = UnicodeAttribute(range_key=True)
-    assistant_type: str = UnicodeAttribute()
-    run_ids: List[str] = ListAttribute()
-    updated_by: str = UnicodeAttribute()
-    created_at: datetime = UTCDateTimeAttribute()
-    updated_at: datetime = UTCDateTimeAttribute()
+    assistant_id = UnicodeAttribute(hash_key=True)
+    thread_id = UnicodeAttribute(range_key=True)
+    assistant_type = UnicodeAttribute()
+    run_ids = ListAttribute()
+    updated_by = UnicodeAttribute()
+    created_at = UTCDateTimeAttribute()
+    updated_at = UTCDateTimeAttribute()
 
 
 class MessageModel(BaseModel):
     class Meta(BaseModel.Meta):
         table_name = "oae-messages"
 
-    thread_id: str = UnicodeAttribute(hash_key=True)
-    message_id: str = UnicodeAttribute(range_key=True)
-    run_id: Optional[str] = UnicodeAttribute(null=True)
-    role: str = UnicodeAttribute()
-    message: str = UnicodeAttribute()
-    created_at: datetime = UTCDateTimeAttribute()
+    thread_id = UnicodeAttribute(hash_key=True)
+    message_id = UnicodeAttribute(range_key=True)
+    run_id = UnicodeAttribute(null=True)
+    role = UnicodeAttribute()
+    message = UnicodeAttribute()
+    created_at = UTCDateTimeAttribute()
