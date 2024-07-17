c3ff33fb1d369d7f26d881f01789a439cbc09a62
Idea Bosque <ideabosque@gmail.com>
2024-07-16 22:18:59 -0700

    Update deploy.

--- openai_assistant_engine/main.py
+++ openai_assistant_engine/main.py
@@ -8,6 +8,7 @@ import logging
 from typing import Any, Dict, List
 
 from graphene import Schema
+
 from silvaengine_dynamodb_base import SilvaEngineDynamoDBBase
 
 from .handlers import handlers_init
@@ -16,7 +17,92 @@ from .schema import Mutations, Query, type_class
 
 # Hook function applied to deployment
 def deploy() -> List:
-    return []
+    return [
+        {
+            "service": "OpenAI Assistant Engine",
+            "class": "OpenaiAssistantEngine",
+            "functions": {
+                "open_assistant_graphql": {
+                    "is_static": False,
+                    "label": "OpenAI Assistant GraphQL",
+                    "query": [
+                        {
+                            "action": "askOpenAi",
+                            "label": "Ask Open AI",
+                        },
+                        {
+                            "action": "liveMessages",
+                            "label": "View Live Messages",
+                        },
+                        {
+                            "action": "lastMessage",
+                            "label": "View Last Message",
+                        },
+                        {
+                            "action": "currentRun",
+                            "label": "View Current Run",
+                        },
+                        {
+                            "action": "assistant",
+                            "label": "View Assistant",
+                        },
+                        {
+                            "action": "assistantList",
+                            "label": "View Assistant List",
+                        },
+                        {
+                            "action": "thread",
+                            "label": "View Thread",
+                        },
+                        {
+                            "action": "threadList",
+                            "label": "View Thread List",
+                        },
+                        {
+                            "action": "message",
+                            "label": "View Message",
+                        },
+                        {
+                            "action": "messageList",
+                            "label": "View Message List",
+                        },
+                    ],
+                    "mutation": [
+                        {
+                            "action": "InsertUpdateAssistant",
+                            "label": "Create Update Assistant",
+                        },
+                        {
+                            "action": "deleteAssistant",
+                            "label": "Delete Assistant",
+                        },
+                        {
+                            "action": "insertUpdateThread",
+                            "label": "Create Update Thread",
+                        },
+                        {
+                            "action": "deleteThread",
+                            "label": "Delete Thread",
+                        },
+                        {
+                            "action": "insertUpdateMessage",
+                            "label": "Create Update Message",
+                        },
+                        {
+                            "action": "deleteMessage",
+                            "label": "Delete Message",
+                        },
+                    ],
+                    "type": "RequestResponse",
+                    "support_methods": ["POST"],
+                    "is_auth_required": False,
+                    "is_graphql": True,
+                    "settings": "open_assistant_engine",
+                    "disabled_in_resources": True,  # Ignore adding to resource list.
+                },
+            },
+        }
+    ]
 
 
 class OpenaiAssistantEngine(SilvaEngineDynamoDBBase):
